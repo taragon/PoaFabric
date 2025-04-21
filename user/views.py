c1bcc5a0164c5ab4ac5e1a25c5d5e4a80f6a87d9
taragon <bethueltaragon@gmail.com>
2025-04-21 12:07:17 +0300

    created function to call homepage

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required 
 
 # Create your views here.
 # This is a view to redirect each individual to the rightful dashboard 
@@ -8,3 +9,23 @@ def login_redirect(request):
         return redirect('recycler_dashboard')
     else:
         return redirect('upcycler_dashboard')
+    
+
+@login_required
+def recycler_dashboard(request):
+    # show recyclable items that is in recyclable.html
+    return render(request, 'user/recycler.html')
+
+@login_required
+def upcycler_dashboard(request):
+    #Show upcycaled items that is in upcycler.html
+    return render(request, 'user/upcycler.html')
+
+# creating homepage function
+def home(request):
+    return render(request, 'user/home.html')
+
+
+    
+
+
