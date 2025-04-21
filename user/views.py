994083addee552991efe9457992413dceefd1e33
taragon <bethueltaragon@gmail.com>
2025-04-21 19:51:49 +0300

    Added a hhtp request

--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required 
 from django.contrib.auth.models import User
