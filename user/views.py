7b0a75a3ba7a713dda5bb67ecf2ecf2d7ffca540
taragon <bethueltaragon@gmail.com>
2025-04-21 14:17:35 +0300

    function to request user to sign up

--- user/views.py
+++ user/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignupForm
+from .models import Profile, Item
 from django.contrib.auth.decorators import login_required 
+
 
 # Create your views here.
 # This is a view to redirect each individual to the rightful dashboard 
@@ -25,6 +29,20 @@ def upcycler_dashboard(request):
 def home(request):
     return render(request, 'user/home.html')
 
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password']
+            )
+            
+            user.profile.role = form.cleaned_data['role']
+            user.profile.save()
+            login(request, user)
+            return redirect('dashboard')
+        
 
     
 
