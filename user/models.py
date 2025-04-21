86bcb4fd21d894b74d76d8519498b5a20de1ea20
taragon <bethueltaragon@gmail.com>
2025-04-21 11:54:50 +0300

    creations of two dashboards for the upcycler and recycler

--- user/models.py
+++ user/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+#This model is for user signup
+
+class Profile(models.Model):
+    ROLE_CHOICES = (
+        ('recycler', 'Recycler'),
+        ('upcycler', 'Upcycler'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+
+    #
