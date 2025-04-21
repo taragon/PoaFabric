4e3cde6bec927dd2a9a51ce20f42a12c867f4b19
taragon <bethueltaragon@gmail.com>
2025-04-21 15:12:11 +0300

    to create path for users in the main urls application

--- user/urls.py
+++ user/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('signup/', views.signup_view, name='signup')
+    path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout')
-    path('dashboard/', views.dashboard, name='dashboard')
+    path('logout/', views.logout_view, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('recycler/', views.recycler_dashboard, name='recycler_dashboard'),
     path( 'upcycler'/ views.upcycler_dashboard, name='upcycler_dashboard'),
 ]
