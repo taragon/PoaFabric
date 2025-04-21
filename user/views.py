21596945b5787f27ddcef92bed4c1629eb53caf0
taragon <bethueltaragon@gmail.com>
2025-04-21 11:30:54 +0300

    Created a view to redirect users to the rightful dashboard

--- user/views.py
+++ user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
+# This is a view to redirect each individual to the rightful dashboard 
+def login_redirect(request):
+    if request.user.profile.role == 'recycler':
+        return redirect('recycler_dashboard')
+    else:
+        return redirect('upcycler_dashboard')
