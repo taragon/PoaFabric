9850aade55754aa67218435eab8306ad518ebda7
taragon <bethueltaragon@gmail.com>
2025-04-23 12:18:36 +0300

    Added a path to the landing page and created html/css on home page

--- PoaFabric/urls.py
+++ PoaFabric/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 
 urlpatterns = [
+    path('', views.home_view, name = 'home'),
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
 ]
