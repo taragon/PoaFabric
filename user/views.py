ac6af28f2a32fed73a67d1df7abc3113f92a835b
taragon <bethueltaragon@gmail.com>
2025-04-23 14:01:46 +0300

    Fixed the bug in view.py allowing us to redirect back to home page

--- user/views.py
+++ user/views.py
@@ -8,30 +8,6 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 
-
-'''
-# Create your views here.
-# This is a view to redirect each individual to the rightful dashboard 
-def login_redirect(request):
-    if request.user.profile.role == 'recycler':
-        return redirect('recycler_dashboard')
-    else:
-        return redirect('upcycler_dashboard')
-    
-
-@login_required
-def recycler_dashboard(request):
-    # show recyclable items that is in recyclable.html
-    return render(request, 'user/recycler.html')
-
-@login_required
-def upcycler_dashboard(request):
-    #Show upcycaled items that is in upcycler.html
-    return render(request, 'user/upcycler.html')
-'''
-
-
-
 # creating homepage function
 def home(request):
     return render(request, 'user/home.html')
@@ -43,25 +19,18 @@ def signup_view(request):
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
             role = form.cleaned_data['role']
-            
-            # Step 1: Create the user
+
             user = User.objects.create_user(username=username, password=password)
-
-            # Step 2: Create a profile linked to that user
             Profile.objects.create(user=user, role=role)
-
-            # Step 3: Log the user in
             login(request, user)
-
-            # Step 4: Redirect to dashboard
             return redirect('dashboard')
         else:
-            return redirect(request, 'home')
+            return render(request, 'user/signup.html', {'form': form})
     else:
         form = SignupForm()
         return render(request, 'user/signup.html', {'form': form})
 
-#This is a login after you sign up 
+
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -73,8 +42,9 @@ def login_view(request):
             return redirect('dashboard')
         else:
             return render(request, 'user/login.html', {'error': 'Invalid credentials'})
-        
-    return redirect(request, 'home')
+
+    return render(request, 'user/login.html')
+
 
 
 # function for loging out
@@ -87,11 +57,12 @@ def logout_view( request):
 def dashboard(request):
     role = request.user.profile.role
     if role == 'recycler':
-        return redirect(recycler_dashboard)
+        return redirect('recycler_dashboard')
     elif role == 'upcycler':
-        return redirect(upcycler_dashboard)
+        return redirect('upcycler_dashboard')
     
 #Function to redirect user to recycler dashboard
+@login_required
 def recycler_dashboard(request):
     items = Item.objects.filter(posted_by=request.user)
     return render(request, 'user/recycler.html', {'items': items})
